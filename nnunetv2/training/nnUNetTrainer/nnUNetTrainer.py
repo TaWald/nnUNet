e9aa12be210d435e6739f63cbd907a48e4fb9ac2
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-09-04 16:27:17 +0200

    Fix deep supervision setting last weight to 0. This is needed for the receptive field experiment: Now only sets to 0 when len == 5.

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -354,13 +354,15 @@ class nnUNetTrainer(object):
 
         # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
         # this gives higher resolution outputs more weight in the loss
-        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
-        weights[-1] = 0
+        if len(deep_supervision_scales) != 0:
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            if len(deep_supervision_scales) == 5:
+                weights[-1] = 0
 
-        # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
-        weights = weights / weights.sum()
-        # now wrap the loss
-        loss = DeepSupervisionWrapper(loss, weights)
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
         return loss
 
     def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
