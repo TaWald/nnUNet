536ea637e6b53af6e5a63be7436435fedc0a4688
c306h <constantin.ulrich@dkfz-heidelberg.de>
2025-05-07 20:42:04 +0200

    wip

--- nnunetv2/training/nnUNetTrainer/pretraining/ssl3d_trainer.py
+++ nnunetv2/training/nnUNetTrainer/pretraining/ssl3d_trainer.py
@@ -1,7 +1,7 @@
 from nnunetv2.training.nnUNetTrainer.pretraining.pretrainedTrainer import PretrainedTrainer_Primus, PretrainedTrainer
 import torch
 
-class PretrainedTrainer_resencL_150ep(PretrainedTrainer):
+class PretrainedTrainer_150ep(PretrainedTrainer):
 
     def __init__(
             self,
@@ -15,7 +15,6 @@ class PretrainedTrainer_resencL_150ep(PretrainedTrainer):
         super().__init__(plans, configuration, fold, dataset_json, device)
         # Can be overriden to train same architecture from scratch.
         self.initial_lr = 1e-3
-        self.enable_deep_supervision = True
         self.warmup_duration_whole_net = 15  # lin increase whole network
 
 class PretrainedTrainer_Primus_150ep(PretrainedTrainer_Primus):
