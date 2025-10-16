981b5e043a6ff4931b7c0682bca9865575bb4cbc
c306h <constantin.ulrich@dkfz-heidelberg.de>
2025-10-16 11:05:03 +0200

    missing no mirroring 1000 ep trainer

--- nnunetv2/training/nnUNetTrainer/pretraining/pretrainedTrainer.py
+++ nnunetv2/training/nnUNetTrainer/pretraining/pretrainedTrainer.py
@@ -659,3 +659,12 @@ class PretrainedTrainer_150ep_nomirroring(PretrainedTrainer):
         mirror_axes = None
         self.inference_allowed_mirroring_axes = None
         return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
+
+class PretrainedTrainer_nomirroring(PretrainedTrainer):
+
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
+            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
